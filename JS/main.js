599c9eba6c233bd182a9fd4cfbe94ef8a54ca96f
Nikita <lun9lol@gmail.com>
2022-11-03 21:05:45 +0200

    пропал свет

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
+const container = document.querySelector("#carousel");
+
 const slides = document.querySelectorAll(".slide");
-const indicators = document.querySelectorAll(".indicator");
 const indicatorsContainer = document.querySelector("#indicators-container");
+const indicators = document.querySelectorAll(".indicator");
 
 const pauseBtn = document.querySelector("#btn-pause");
 const prevBtn = document.querySelector("#btn-prev");
@@ -14,13 +16,15 @@ const CODE_SPACE = "Space";
 let currentSlide = 0;
 let isPlayng = true;
 let timerID = null;
+let startPosX = null;
+let endPosX = null;
 
 function gotoNth(n) {
   slides[currentSlide].classList.toggle("active");
   indicators[currentSlide].classList.toggle("active");
   currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
-  indicators[currentSlide].classList.toggle("active");
   slides[currentSlide].classList.toggle("active");
+  indicators[currentSlide].classList.toggle("active");
 }
 
 function gotoPrev() {
@@ -43,7 +47,7 @@ function play() {
   timerID = setInterval(gotoNext, 2000);
 }
 
-function pausePlayHandler() {
+function pausePlay() {
   isPlayng ? pause() : play();
 }
 
@@ -67,15 +71,42 @@ function indicate(e) {
 }
 
 function pressKey(e) {
-  if (e.code === CODE_LEFT_ARROW) return prev();
-  if (e.code === CODE_RIGHT_ARROW) return next();
-  if (e.code === CODE_SPACE) return pausePlay();
+  if (e.code === CODE_LEFT_ARROW) prev();
+  if (e.code === CODE_RIGHT_ARROW) next();
+  if (e.code === CODE_SPACE) pausePlay();
 }
 
-pauseBtn.addEventListener("click", pausePlayHandler);
+function swipeStart(e) {
+  if (e instanceof MouseEvent) {
+    startPosX = e.pageX;
+
+    return;
+  }
+
+  if (e instanceof TouchEvent) {
+    startPosX = e.changedTouches[0].pageX;
+  }
+}
+
+function swipeEnd(e) {
+  if (e instanceof MouseEvent) {
+    endPosX = e.pageX;
+  } else if (e instanceof TouchEvent) {
+    endPosX = e.changedTouches[0].pageX;
+  }
+
+  if (endPosX - startPosX > -100) prev();
+  if (endPosX - startPosX < 100) next();
+}
+
+pauseBtn.addEventListener("click", pausePlay);
 prevBtn.addEventListener("click", prev);
 nextBtn.addEventListener("click", next);
 indicatorsContainer.addEventListener("click", indicate);
+container.addEventListener("touchstart", swipeStart);
+container.addEventListener("mousedown", swipeStart);
+container.addEventListener("touchend", swipeEnd);
+container.addEventListener("mouseup", swipeEnd);
 document.addEventListener("keydown", pressKey);
 
 timerID = setInterval(gotoNext, 2000);
